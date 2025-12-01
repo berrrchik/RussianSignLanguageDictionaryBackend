8d971a31f490e93915f98deb871c204196eaa7e7
berrrchik <berrrchik@mail.ru>
2025-12-01 23:56:06 +0300

    feat: add Flask application entry point

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(os.getenv('FLASK_PORT', 5001))
+    app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=port)
+
